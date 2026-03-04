e8d7763b160a00d04d542ae9b246497ee5af8fc2
EC2 Default User <ec2-user@ip-172-31-42-94.ap-south-1.compute.internal>
2026-03-04 23:58:51 +0000

    Added appspec.yml and start script

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello from my Node.js app running in AWS CodeBuild!");
-``
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello! Your Node.js App is LIVE on EC2 through CodePipeline & CodeDeploy ");
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Server running on http://3.108.223.222:3000");
+});
